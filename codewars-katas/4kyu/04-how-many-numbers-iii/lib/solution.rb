3fe9996fbc0abebb09330ab50b3e5a742e327dd9
Martin Klapper <martinklapper@tutanota.com>
2023-10-30 19:32:53 +0100

    move and rename of misnamed files

--- codewars-katas/4kyu/04-how-many-numbers-iii/lib/solution.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-def solution(cross_sum, n_digits)
-  # guard for impossibles, eg. 3 digits and cross_sum 20
-  return [] if ((9 * n_digits) <= cross_sum) || ((1 * n_digt) >= cross_sum)
-
-  return [result.size, result.first, result.last]
-end
