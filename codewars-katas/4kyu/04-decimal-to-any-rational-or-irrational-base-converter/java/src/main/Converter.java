41696a929fa45920089b3373cfe5f3da73e6e023
Martin Klapper <martinklapper@tutanota.com>
2023-11-01 20:14:23 +0100

    working but weirdly sometimes off by 10 when converting to ASCII

--- codewars-katas/4kyu/04-decimal-to-any-rational-or-irrational-base-converter/java/src/main/Converter.java
+++ codewars-katas/4kyu/04-decimal-to-any-rational-or-irrational-base-converter/java/src/main/Converter.java
@@ -1,13 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Converter {
+
+  // Convert numbers between 10-36 to A-Z
+  private static String getStringFromInt(int i) {
+
+    // use ASCII table for conversion
+    if (i >= 10 && i <= 36) {
+      System.out.println(List.of("Converting", i, "to", String.valueOf((char)(i + 'A'))));
+      return String.valueOf((char)(i + 'A'));
+    }
+
+    return Integer.toString(i);
+  }
 
 	public static String converter(double n, int decimals, double base) {
 
-    // decimals >= 0
-    // 2 <= base <= 36 (1 not needed)
-    // 36 Base of numeration 0-Z ; "A(for11*base [base > 10]).Z02" is 'possible'
-    // --> Always prioritize using the most significant digit that is possible!
+    System.out.println(List.of(n, decimals, base));
+    ArrayList<String> result = new ArrayList<String>();
+    boolean negative_flag = false;
 
-    // output formated with "XXXX.decimals" (0 if < 1)
-    return "";
+    // n * -1 if n < 0, to also account for negatives
+    if (n < 0) {
+      negative_flag = true;
+      n *= -1;
+    }
+
+    // find highest exponent k for which Math.pow(base, k) <= n; logbase (n) = log10(n) / log10(b)
+    int k = (int)((Math.floor(Math.log(n) / Math.log(base))) + 1);
+
+    // decreasing k, find the next digit for result and adjust n. Add . when needed to separate before and after comma
+    for (int i = k-1; i > (-1) * decimals - 1; i--) {
+      if (result.size() == k) {
+        result.add(".");
+      }
+
+      int digit = (int)(Math.floor(n / Math.pow(base, i)) % base);
+      n -= digit * Math.pow(base, i);
+      result.add(Converter.getStringFromInt(digit));
+    }
+
+    if (negative_flag) {
+      result.add(0, "-");
+    }
+
+    return String.join("", result);
   }
 }
--- codewars-katas/4kyu/04-decimal-to-any-rational-or-irrational-base-converter/java/src/test/ConverterTest.java
+++ codewars-katas/4kyu/04-decimal-to-any-rational-or-irrational-base-converter/java/src/test/ConverterTest.java
@@ -8,5 +8,7 @@ public class ConverterTest {
 	@Test
 	public void test1() {
 	  assertEquals("103", Converter.converter(13, 0, Math.PI));
+    assertEquals("N.8000", Converter.converter(13.5, 4, 16.0));
+    assertEquals("-3PL0U.L0000000", Converter.converter(-867832.5, 8, 22.0));
 	}
 }
