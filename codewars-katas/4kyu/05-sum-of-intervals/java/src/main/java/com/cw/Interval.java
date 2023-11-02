f62912a7d982c3280de79800304d895e0047451f
Martin Klapper <martinklapper@tutanota.com>
2023-11-02 21:46:51 +0100

    also track binaries

--- codewars-katas/4kyu/05-sum-of-intervals/java/src/main/java/com/cw/Interval.java
+++ codewars-katas/4kyu/05-sum-of-intervals/java/src/main/java/com/cw/Interval.java
@@ -3,6 +3,9 @@ package com.cw;
 public class Interval {
 
     public static int sumIntervals(int[][] intervals) {
+
+      // to identify overlapping in only one pass of the nested array
+
         // TODO: implement this method
         return -1;
     }
