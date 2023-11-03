949ae10eaf97fe4af0eae83eba632961d7747f80
Martin Klapper <martinklapper@tutanota.com>
2023-11-03 10:09:05 +0100

    finished in O(n) i think

--- codewars-katas/4kyu/05-sum-of-intervals/java/src/main/java/com/cw/Interval.java
+++ codewars-katas/4kyu/05-sum-of-intervals/java/src/main/java/com/cw/Interval.java
@@ -1,12 +1,40 @@
 package com.cw;
 
+import java.util.Arrays;
+
 public class Interval {
 
-    public static int sumIntervals(int[][] intervals) {
+  public static int sumIntervals(int[][] intervals) {
 
-      // to identify overlapping in only one pass of the nested array
-
-        // TODO: implement this method
-        return -1;
+    if (intervals.length == 0) {
+      return 0;
     }
+
+    // sort outer array as to only have to account for 'ascending' overlap
+    Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+    int result = 0;
+    int[] current_interval = intervals[0];
+
+    // iterate outer array
+    for (int i = 1; i < intervals.length; i++) {
+
+      int[] next_interval = intervals[i];
+
+      if (current_interval[1] >= next_interval[0]) {
+        // if overlapping, only keep the bigger right limit
+        current_interval[1] = Math.max(current_interval[1], next_interval[1]);
+      }
+      else {
+        // if not overlapping, add the distance of current and prepare for next iteration
+        result += current_interval[1] - current_interval[0];
+        current_interval = next_interval;
+      }
+    }
+
+    // add size of last considered interval to result
+    result += current_interval[1] - current_interval[0];
+
+    return result;
+  }
 }
